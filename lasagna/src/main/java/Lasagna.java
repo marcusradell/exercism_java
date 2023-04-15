6eb684cbf0e82872c8478a7528f63462f14f6614
Marcus Rådell <marcus@radell.net>
2023-04-15 22:34:27 +0200

    implement first two methods for lasagna lab

---
+++ lasagna/src/main/java/Lasagna.java
@@ -0,0 +1,13 @@
+public class Lasagna {
+    public int expectedMinutesInOven() {
+        return 40;
+    }
+
+    public int remainingMinutesInOven(int minutesInOven) {
+        return expectedMinutesInOven() - minutesInOven;
+    }
+
+    // TODO: define the 'preparationTimeInMinutes()' method
+
+    // TODO: define the 'totalTimeInMinutes()' method
+}
